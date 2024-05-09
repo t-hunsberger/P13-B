7711aa06b8aa65b40b3bcec7b00ed401e9d8411e
t-hunsberger <t-hunsberger@onu.edu>
2024-05-08 21:33:38 -0400

    13B-1

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,40 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool isLeapYear(int year) {
+	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+}
+
 int main() {
-	string name;
-	cout << "What is your name " << endl;
-	cin >> name;
+	string input;
+	int year;
 
-	if (name == "nat" || name == "nataleigh") {
-		cout << "hello " << name << " that is a very pretty name!!";
-	}
-	else if (name == "tucker") {
-		cout << "ewww";
-	}
-	else {
-		cout << "hello " << name << " why would your parents not name you nataleigh";
+	cout << "Enter a yaer or Q to quit: ";
+	while (cin >> input) {
+		if (input == "Q" || input == "q")
+			break;
 
+		try {
+			year = stoi(input);
+			if (year < 1582) {
+				cout << "Year must be greater than or equal to 1582." << endl;
+				cout << "Enter a year or Q to quit: ";
+				continue;
+			}
+
+			if (isLeapYear(year))
+				cout << year << " is a leap year." << endl;
+			else
+				cout << year << " is not a leap year." << endl;
+		}
+		catch (invalid_argument&) {
+			cout << "Invalid input. Pleae enter a valid year or Q to quit." << endl;
+		}
+
+		cout << "Enter a year or Q to quit: ";
 	}
 
+	return 0;
 }
